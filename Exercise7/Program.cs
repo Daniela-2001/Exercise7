a313abfbe147cfdf065ac6e88f42a699987e2ce4
Daniela <Daniela@DESKTOP-P4BHM2A>
2023-07-19 10:37:47 +0100

    Adicionar arquivos de projeto.

---
+++ Exercise7/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Exercise7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String Nome; //Variável que armazena o nome do usuário
+
+            Console.WriteLine("Escreva seu nome: ");
+
+            Nome = Console.ReadLine();
+
+            Console.WriteLine("");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Meu nome é");
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(Nome);
+            Console.ReadKey();
+
+            
+        }
+    }
+}
